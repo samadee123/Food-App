1dc6b58063627004a798b1d2b4c69af347672873
samadee123 <samadeekularatne78@gmail.com>
2024-02-08 02:08:42 +0530

    Dashboard Backend

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.example.myapplication.Adapter.BestFoodsAdapter;
+import com.example.myapplication.Adapter.CategoryAdapter;
+import com.example.myapplication.Domain.Category;
 import com.example.myapplication.Domain.Foods;
 import com.example.myapplication.Domain.Location;
 import com.example.myapplication.Domain.Price;
@@ -36,6 +39,7 @@ public class MainActivity extends BaseActivity {
         initTime();
         initPrice();
         initBestFood();
+        initCategory();
     }
 
     private void initBestFood() {
@@ -56,6 +60,33 @@ public class MainActivity extends BaseActivity {
                         binding.bestFoodView.setAdapter(adapter);
                     }
                     binding.progressBarBestFood.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initCategory() {
+        DatabaseReference myRef=database.getReference("Category");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<Category> list=new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        list.add(issue.getValue(Category.class));
+                    }
+                    if (list.size() > 0){
+                        binding.categoryView.setLayoutManager(new GridLayoutManager(MainActivity.this,4));
+                        RecyclerView.Adapter adapter = new CategoryAdapter(list);
+                        binding.categoryView.setAdapter(adapter);
+                    }
+                    binding.progressBarCategory.setVisibility(View.GONE);
                 }
             }
 
